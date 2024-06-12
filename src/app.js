cc9f5e192732c75e13f636be23bb0593a89e639e
Monesillo <skind.duran.f@gmail.com>
2024-06-11 18:12:22 -0600

    Avance en js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  window.onload = () => {
+    document.querySelector(".card").classList.add(genRandSuit());
+    document.querySelector(".card").innerHTML = genRandNumb();
+  };
+  
+  let genRandNumb = () => {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexNumbers = Math.floor(Math.random() * numbers.length);
+    return numbers[indexNumbers];
+  };
+  
+  let genRandSuit = () => {
+    let suit = ["diamond", "spade", "heart", "club"];
+    let indexSuit = Math.floor(Math.random() * suit.length);
+    return suit[indexSuit];
+  }
+}
